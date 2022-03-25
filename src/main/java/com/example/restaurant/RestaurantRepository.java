0c8129807950b78a3c25e955cc92f4ca0bfef964
mgreyes089 <98517687+mgreyes089@users.noreply.github.com>
2022-03-25 13:56:49 +0100

    Con postman y XAMPP

---
+++ src/main/java/com/example/restaurant/RestaurantRepository.java
@@ -0,0 +1,8 @@
+package com.example.restaurant;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends CrudRepository<Restaurant, String> {
+}
